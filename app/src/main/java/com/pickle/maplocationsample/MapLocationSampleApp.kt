150af12dfba0af9d0620f46792ae445197712191
akhu <anthony.dacr@gmail.com>
2020-01-26 14:59:42 +0100

    Working as expected

--- app/src/main/java/com/pickle/maplocationsample/MapLocationSampleApp.kt
+++ app/src/main/java/com/pickle/maplocationsample/MapLocationSampleApp.kt
@@ -1,9 +1,13 @@
 package com.pickle.maplocationsample
 
 import android.app.Application
+import timber.log.Timber
 
 class MapLocationSampleApp : Application(){
 
-
+    override fun onCreate() {
+        super.onCreate()
+        Timber.plant(Timber.DebugTree())
+    }
 
 }
